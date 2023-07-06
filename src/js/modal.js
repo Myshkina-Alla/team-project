f741b2cd38b18549feaa648d4435efa20e324e4f
wordisstuff <wordisstuff@gmail.com>
2023-07-06 09:59:35 +0300

    add name data in dataAtrebute

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,10 @@
 (() => {
   const refs = {
-    openModalBtnVegetables: document.querySelector('[vegetables-modal-open]'),
+    openModalBtnVegetables: document.querySelector('[data-vegetables-modal-open]'),
     // openModalBtnHederMenu: document.querySelector(
     //   '[heder-menu-modal-open]'
     // ),
-    openModalBtnHeder: document.querySelector('[heder-modal-open]'),
+    openModalBtnHeder: document.querySelector('[data-heder-modal-open]'),
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
